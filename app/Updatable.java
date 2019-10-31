eba2c37477ad25d28d9a6f07f61d11bcc4fbde5b
urmzd <45431570+urmzd@users.noreply.github.com>
2019-10-30 23:25:53 -0300

    Add comments.

--- app/Updatable.java
+++ app/Updatable.java
@@ -1,6 +1,13 @@
 package com.flappybird.app;
 
+/**
+ * The Updateable intefaces defines methods used when changing the position, velocity,
+ * rotation or acceleration of an Object.
+ */
 public interface Updatable {
 
+    /**
+     * The update method() is used to update an Objects attributes.
+     */
     public void update();
 }
