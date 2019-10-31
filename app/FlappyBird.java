8cf4aaf7fbba19ae8fd8b66871371396e5e107a0
urmzd <45431570+urmzd@users.noreply.github.com>
2019-10-30 23:18:40 -0300

    Update FlappyBird.java

--- app/FlappyBird.java
+++ app/FlappyBird.java
@@ -1,5 +1,9 @@
 package com.flappybird.app;
-
+/**
+ * The FlappyBird class runs the a clone of the popular mobile-game Flappy Bird.
+ * 
+ * @author Urmzd Mukhammadnaim
+ */
 public class FlappyBird { 
     public static void main(String[] args){
         GameCanvas.main(args);
