80870a41dcb8c24f73f2eece94edbcf2fbe676c1
Urmzd <urmzds@gmail.com>
2021-12-29 01:34:02 -0500

    build - modularize flappybird

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module flappybird {
+  requires transitive javafx.graphics;
+  requires transitive javafx.base;
+
+  opens com.urmzd.flappybird to javafx.base, javafx.graphics;
+
+  exports com.urmzd.flappybird;
+}
